b45c31baaaf6a2d8906a6e5bc69483b2633f521b
Santiago Villa <santiago.villa@radiumrocket.com>
2020-11-03 00:40:29 -0300

    NEWS: Task 05. Creation of the index.js with the import of the elements of each input

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const age = document.getElementById('age');
+const idCard = document.getElementById('idCard');
+const phoneNumber = document.getElementById('phoneNumber');
+const address = document.getElementById('address');
+const city = document.getElementById('city');
+const zip = document.getElementById('zip');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+});
