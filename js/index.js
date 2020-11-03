a1869b417296db603c1e60f1246c1d82a9388707
Santiago Villa <santiago.villa@radiumrocket.com>
2020-11-03 15:48:19 -0300

    NEWS: Task 05. Username validation done

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const form = document.getElementById('form');
 const username = document.getElementById('username');
+const email = document.getElementById('email');
 const submit = document.getElementById('submit');
 
 username.focus();
@@ -10,10 +11,42 @@ form.addEventListener('submit', e => {
 
 username.addEventListener('blur', function() {
     const usernameValue = username.value.trim();
-
     if (usernameValue.length < 7 || usernameValue.indexOf(' ') === -1) {
-        console.log('Username invalid')
+        setErrorFor(username);
     } else {
-        console.log('Username valid')
+        setSuccessFor(username);
     }
 })
+
+username.addEventListener('focus', function() {
+    setSuccessFor(username);
+})
+
+email.addEventListener('blur', function() {
+    const emailValue = email.value.trim();
+    if (emailValue.length < 7 || emailValue.indexOf(' ') === -1) {
+        setErrorFor(email);
+    } else {
+        setSuccessFor(email);
+    }
+})
+
+email.addEventListener('focus', function() {
+    setSuccessFor(email);
+})
+
+function setErrorFor(input) {
+    const formField = input.parentElement;
+    const errorElement = formField.querySelector('p');
+    const formInput = formField.querySelector('input');
+    errorElement.style.visibility = 'visible';
+    formInput.style.border = '2px solid rgb(255, 34, 34)';
+}
+
+function setSuccessFor(input) {
+    const formField = input.parentElement;
+    const errorElement = formField.querySelector('p');
+    const formInput = formField.querySelector('input');
+    errorElement.style.visibility = 'hidden';
+    formInput.style.border = '1px solid rgb(23, 219, 23)';
+}
