ef9f4844106b55cc8e69af798ae350e3227a7c81
Santiago Villa <santiago.villa@radiumrocket.com>
2020-11-03 21:27:51 -0300

    NEWS: Task 05. Improvements in username, email and password validations

--- js/index.js
+++ js/index.js
@@ -2,7 +2,9 @@ const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
+const age = document.getElementById('age');
 const submit = document.getElementById('submit');
+const error = document.getElementsByClassName('error');
 
 username.focus();
 
@@ -10,51 +12,76 @@ form.addEventListener('submit', e => {
     e.preventDefault();
 });
 
+
+// Username Validation
 username.addEventListener('blur', function() {
     const usernameValue = username.value.trim();
-    if (usernameValue.length < 7 || usernameValue.indexOf(' ') === -1) {
+    if (usernameValue.length < 7) {
+        error[0].style.visibility = 'visible';
+        error[0].innerHTML = 'It must have more than 6 letters.';
+        setErrorFor(username);
+    } else if (usernameValue.indexOf(' ') === -1) {
+        error[0].style.visibility = 'visible';
+        error[0].innerHTML = 'It must have a space between names.';
         setErrorFor(username);
     } else {
         setSuccessFor(username);
     }
 })
-
 username.addEventListener('focus', function() {
     setSuccessFor(username);
 })
 
-
+// Email Validation
 email.addEventListener('blur', function() {
     const emailValue = email.value.trim();
     if (!isEmail(emailValue)) {
+        error[1].style.visibility = 'visible';
+        error[1].innerHTML = 'It must be a valid email.';
         setErrorFor(email);
     } else {
         setSuccessFor(email);
     }
 })
-
 email.addEventListener('focus', function() {
     setSuccessFor(email);
 })
 
-
-
+// Password Validation
 password.addEventListener('blur', function() {
     const passwordValue = password.value.trim();
-    var regularExpression = /[^0-9a-zA-Z]/;
-    if (passwordValue.length < 8 || regularExpression.test(passwordValue)) {
+    var regNumExp = /[0-9]/;
+    var regAlfExp = /[a-zA-Z]/;
+    if (passwordValue.length < 8) {
+        error[2].style.visibility = 'visible';
+        error[2].innerHTML = 'It must be at least 8 characters long.';
+        setErrorFor(password);
+    } else if (!regAlfExp.test(passwordValue) || !regNumExp.test(passwordValue)) {
+        error[2].style.visibility = 'visible';
+        error[2].innerHTML = 'It must have numbers and letters.';
         setErrorFor(password);
     } else {
         setSuccessFor(password);
     }
 })
-
 password.addEventListener('focus', function() {
     setSuccessFor(password);
 })
 
-
-
+// Age Validation
+age.addEventListener('blur', function() {
+    const ageValue = age.value.trim();
+    if (ageValue < 18 || !Number.isInteger(Number(ageValue))) {
+        error[3].style.visibility = 'visible';
+        error[3].innerHTML = 'Must be 18 or over.';
+        setErrorFor(age);
+    } else {
+        setSuccessFor(age);
+    }
+})
+age.addEventListener('focus', function() {
+    setSuccessFor(age);
+})
 
 
 
@@ -64,9 +91,7 @@ password.addEventListener('focus', function() {
 
 function setErrorFor(input) {
     const formField = input.parentElement;
-    const errorElement = formField.querySelector('p');
     const formInput = formField.querySelector('input');
-    errorElement.style.visibility = 'visible';
     formInput.style.border = '2px solid rgb(255, 34, 34)';
 }
 
@@ -74,7 +99,7 @@ function setSuccessFor(input) {
     const formField = input.parentElement;
     const errorElement = formField.querySelector('p');
     const formInput = formField.querySelector('input');
-    errorElement.style.visibility = 'hidden';
+    errorElement.innerHTML = '';
     formInput.style.border = '1px solid rgb(23, 219, 23)';
 }
 
